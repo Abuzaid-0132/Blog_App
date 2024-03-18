16f182a007c255902cdb8dedeb9e5ce26e5fff97
Abuzaid-0132 <mohdabuzaid0132@gmail.com>
2024-03-19 04:03:31 +0530

    fixing home route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SingUp />} />
